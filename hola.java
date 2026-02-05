63041b26dc15535e3ab77ae7b587810dca437038
Hector <hectormartinezcabedo@gmail.com>
2026-02-05 12:43:27 +0100

    Prueba cambios

---
+++ hola.java
@@ -0,0 +1,20 @@
+public class hola {
+
+    public static void main(String[] args) {
+        Persona persona1 = new Persona("Juan", 30);
+        persona1.saludar();
+    }
+}
+class Persona{
+    String nombre;
+    int edad;
+
+    public Persona(String nombre, int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void saludar(){
+        System.out.println("Hola, mi nombre es " + nombre + " y tengo " + edad + " años.");
+    }
+}
