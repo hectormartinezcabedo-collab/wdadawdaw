30fa89cec29d5b29bc1797819b470ced2573f962
HectorMC <hectormartinezcabedo@gmail.com>
2026-02-05 12:46:55 +0100

    Correct constructor name in Alumno class
    Fix constructor name from 'Persona' to 'Alumno' in Alumno class.

--- hola.java
+++ hola.java
@@ -18,3 +18,17 @@ class Persona{
         System.out.println("Hola, mi nombre es " + nombre + " y tengo " + edad + " años.");
     }
 }
+
+class Alumno{
+    String nombre;
+    int edad;
+
+    public Persona(String nombre, int edad){
+        this.nombre = nombre;
+        this.edad = edad;
+    }
+
+    public void saludar(){
+        System.out.println("Hola, mi nombre es " + nombre + " y tengo " + edad + " años.");
+    }
+}
